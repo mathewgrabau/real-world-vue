5eac3db25af15e21a8de4501159e7a64395ad34f
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-02-07 01:43:04 -0600

    get Event API added, integrated to the component

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Showing event #{{ id }}</h1>
+  <h1>Showing event #{{ id }} {{ event.title }}</h1>
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: Object
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        // Setting in the received response.
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 }
 </script>

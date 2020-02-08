696117d738a69dd68699d1b6e780a0f6ef3654bd
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-02-07 22:24:55 -0600

    Added the elements to display the event details.

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1>Showing event #{{ id }} {{ event.title }}</h1>
+  <div>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by: {{ event.organizer }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+    <address>{{ event.location }}</address>
+    <h2>Event Details</h2>
+    <p>{{ event.description }}</p>
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -19,7 +45,7 @@ export default {
         this.event = response.data
       })
       .catch(error => {
-        console.log(error)
+        console.log('There was an error:', error.response)
       })
   }
 }

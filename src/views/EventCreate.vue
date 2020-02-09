6634410a17128e0a77d0394ec5488e2e5dec52b3
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-02-09 16:27:15 -0600

    Mapped a mixed property.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Create Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.name }} ({{ user.id }})</p>
+    <p>{{ localComputed }}</p>
     <ul>
       <li v-for="category in categories" :key="category">{{ category }}</li>
     </ul>
@@ -14,6 +15,12 @@ import { mapState } from 'vuex'
 export default {
   // Using the mapState to replace manually implementing computed properties.
   // Strings to do mapping works nicely as well.
-  computed: mapState(['user', 'categories'])
+  // The object spread operator is ... and it allows mixing the local and mapped
+  computed: {
+    localComputed() {
+      return 'Here is something'
+    },
+    ...mapState(['user', 'categories'])
+  }
 }
 </script>

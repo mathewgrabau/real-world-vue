e0d9691f47324ebca1a41ed2dfaee46b548fec44
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-02-09 16:18:19 -0600

    Event simpler mapping (string based); rendering categories for a test.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <h1>Create Event, {{ userName }}</h1>
-    <p>This event was created by {{ userName }} ({{ userId }})</p>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }} ({{ user.id }})</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
   </div>
 </template>
 
@@ -10,10 +13,10 @@ import { mapState } from 'vuex'
 
 export default {
   // Using the mapState to replace manually implementing computed properties.
+  // Strings to do mapping works nicely as well.
   computed: mapState({
-    userName: state => state.user.name,
-    userId: state => state.user.id,
-    categories: state => state.categories
+    user: 'user',
+    categories: 'categories'
   })
 }
 </script>

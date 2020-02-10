e11bd34805dc481fb4ce85e369c242fa5c99b2e3
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-02-09 23:53:40 -0600

    Added the commit of an action and mutation to create an event.

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
@@ -9,6 +10,7 @@ export default new Vuex.Store({
       id: 'abc123',
       name: 'Mathew Grabau'
     },
+    events: [],
     categories: [
       'sustainability',
       'nature',
@@ -19,12 +21,27 @@ export default new Vuex.Store({
       'community'
     ]
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    // event is the payload
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  // Always want to put mutations inside actions (according to the core Vue team). Idea is to increase scalability of the codebase.
+  // NOTE: actions get dispatched from a component. The action takes care of committing the mutation.
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event) // push it into the database here
+      commit('ADD_EVENT', event) // Commit the event
+    }
+  },
   modules: {},
   getters: {
     categoriesLength: state => {
       return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
     }
   }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,26 +1,107 @@
 <template>
   <div>
-    <h1>Create Event, {{ user.name }}</h1>
-    <p>This event was created by {{ user.name }} ({{ user.id }})</p>
-    <p>There are {{ categoriesLength }} categories available</p>
-    <ul>
-      <li v-for="category in categories" :key="category">{{ category }}</li>
-    </ul>
+    <h1>Create Event</h1>
+    <form @submit.prevent="createEvent">
+      <label for="">Select a category</label>
+      <select v-model="event.category">
+        <option v-for="category in categories" :key="category">{{
+          category
+        }}</option>
+      </select>
+      <h3>Name and describe your event</h3>
+      <div class="field">
+        <label for="">Title</label>
+        <input
+          type="text"
+          placeholder="Add an event title"
+          v-model="event.title"
+        />
+      </div>
+      <div class="field">
+        <label for="">Description</label>
+        <input
+          type="text"
+          placeholder="Add a description"
+          v-model="event.description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label for="">Location</label>
+        <input
+          type="text"
+          placeholder="Add a location"
+          v-model="event.location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label for="">Date</label>
+        <DatePicker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label for="">Select a time</label>
+        <select name="" id="" v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+// import { mapState } from 'vuex'
+import DatePicker from 'vuejs-datepicker'
 
 export default {
+  components: {
+    DatePicker
+  },
+  data() {
+    const times = []
+    for (let i = 1; i <= 24; ++i) {
+      times.push(i + ':00')
+    }
+
+    return {
+      times,
+      event: this.createFreshEvent(),
+      categories: this.$store.state.categories
+    }
+  },
+  methods: {
+    createEvent() {
+      // Takes care of invoking the action needed to add the event into the database
+      this.$store.dispatch('createEvent', this.event)
+    },
+    createFreshEvent() {
+      const user = this.$store.state.user
+      return {
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
+  },
   // Using the mapState to replace manually implementing computed properties.
   // Strings to do mapping works nicely as well.
   // The object spread operator is ... and it allows mixing the local and mapped
   computed: {
     categoriesLength() {
       return this.$store.getters.categoriesLength
-    },
-    ...mapState(['user', 'categories'])
+    }
   }
 }
 </script>
+
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
